f0c673445fb7ee865374b0a3bba4cee3aea03d0a
JustPowerful <ahmedaminedoudech@gmail.com>
2025-11-29 00:48:06 +0100

    fix the consultation section

--- app/_component/timeline.tsx
+++ app/_component/timeline.tsx
@@ -54,13 +54,14 @@ const TimelineItem = ({
       { threshold: 0.2 }
     );
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, []);
