ea474f2db3272f33e9feb8a7ae9c055f12660424
JustPowerful <ahmedaminedoudech@gmail.com>
2025-11-29 21:52:24 +0100

    add 404 easter egg page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import notFoundImage from "@/assets/404.png";
+import Image from "next/image";
 
 const NotFound = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      Oops! Page is not found!
+      <Image
+        src={notFoundImage}
+        alt="404 Easter egg image"
+        width={800}
+        height={800}
+      />
     </div>
   );
 };
